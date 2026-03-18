6f0ebe2d82aa1339fca2e1046a66bbcfec1b92d8
Dijo Davis C <dijodijodavis3@gamil.com>
2026-03-18 14:07:29 +0530

    nxt

--- langauge_fundementels/task/operators/task31.py
+++ langauge_fundementels/task/operators/task31.py
@@ -27,3 +27,39 @@ lst_a[0]["car"]="bmw"
 """
 4. Write a Python program to perform deep copy using the deepcopy() function from the copy module.
 """
+from copy import deepcopy
+lst=[
+    {"car":"benz","model":2000,"price":1000000},
+    {"car":"bmw","model":2002,"price":1500000},
+]
+newlst=deepcopy(lst)
+newlst[0]["car"]="thar"
+# print(lst,"\n",newlst)
+"""
+5. Create a nested list and perform deep copy. Modify the inner list and show that the original list remains unchanged.
+"""
+from copy import deepcopy
+lst=[
+    {"car":"benz","model":2000,"price":1000000},
+    {"car":"bmw","model":2002,"price":1500000},
+]
+newlst=deepcopy(lst)
+newlst[0]["model"]="2006"
+# print(lst,"\n",newlst)
+"""
+6. Write a program to compare shallow copy and deep copy using nested lists.
+"""
+from copy import deepcopy
+lst1=[
+    {"car":"benz","model":2000,"price":1000000},
+    {"car":"bmw","model":2002,"price":1500000},
+]
+# lst_copy=lst1.copy()
+# lst_copy[0]["car"]="bmw"
+# print(lst1,"\n",lst_copy)
+# lst_copyy=deepcopy(lst1)
+# lst_copyy[0]["car"]="bmw"
+# print(lst1,"\n",lst_copyy)
+"""
+7. Create a dictionary containing a list as a value. Perform shallow copy and modify the list. Print both dictionaries.
+"""
